3764a337530ff575ee73591726956c46465a2270
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2021-11-07 14:30:24 -0500

    robot name update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("the fight has begun!");
+}
+fight();
+var playerName=window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+console.log("This logas a string,good for leaving yourself a massage");
+//this will od math and log 20
+console.log(10+10);
+//what is this
+console.log("our robot's name is" + playerName);
+var name = "your name";
+console.log(name);
